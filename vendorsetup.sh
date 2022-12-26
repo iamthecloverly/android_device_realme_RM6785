c93ace3502bc0a13ae628748494aff7cf9daac1e
SriBalaji <iam.thecloverly@pm.me>
2022-12-26 19:10:18 +0530

    vendorsetup.sh: add patch for external/faceunlock

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,7 @@ git clone https://github.com/realme-mt6785-devs/android_device_mediatek_sepolicy
 git clone https://github.com/realme-mt6785-devs/android_device_realme_RM6785-kernel.git device/realme/RM6785-kernel
 git clone https://github.com/realme-mt6785-devs/vendor_mediatek_opensource_interfaces.git vendor/mediatek/opensource/interfaces
 
+cd external/faceunlock && wget https://pastebin.com/raw/EbesA2Wm && patch -p1  < EbesA2Wm
 #patch for brightness slider 
 cd frameworks/base && wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices-a12l.patch
 patch -p1 < *.patch && cd -
