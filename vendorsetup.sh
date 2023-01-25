2fcbc502e87b4e94d83101aef516571942ed9b2e
SriBalaji <iam.thecloverly@pm.me>
2023-01-25 22:34:14 +0530

    RM6785: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,7 +12,9 @@ git clone https://github.com/realme-mt6785-devs/android_kernel_realme_mt6785.git
 git clone https://github.com/realme-mt6785-devs/proprietary_vendor_realme-firmware realme/realme-firmware
 git clone https://github.com/realme-mt6785-devs/android_device_mediatek_sepolicy.git device/mediatek/sepolicy
 git clone https://github.com/realme-mt6785-devs/android_device_realme_RM6785-kernel.git device/realme/RM6785-kernel
+git clone https://github.com/realme-mt6785-devs/vendor_mediatek_opensource_interfaces.git vendor/mediatek/opensource/interfaces
 
+cd external/faceunlock && wget https://pastebin.com/raw/EbesA2Wm && patch -p1  < EbesA2Wm
 #patch for brightness slider 
 cd frameworks/base && wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices-a12l.patch
 patch -p1 < *.patch && cd -
